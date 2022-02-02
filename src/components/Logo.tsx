ebdbbae44faf9410f7a5e81a7a474fe29e49b503
Rupert Redington <rupert@lola.tech>
2022-02-02 20:17:04 +0000

    refactor: plain css with pico

--- src/components/Logo.tsx
+++
@@ -1,7 +0,0 @@
-import { Image } from '@chakra-ui/react';
-
-const Logo = () => (
-  <Image src="/logoIcon.svg" alt="logo" height="2rem" shadow="dark-lg" />
-);
-
-export default Logo;
--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -1,60 +1,10 @@
-import {
-  ChakraProps,
-  Heading,
-  chakra,
-  UnorderedList,
-  OrderedList,
-  ListItem,
-} from '@chakra-ui/react';
-import NextImage, { ImageProps } from 'next/image';
-
-const Image = chakra(NextImage, {
-  shouldForwardProp: (prop) =>
-    [
-      'width',
-      'height',
-      'src',
-      'alt',
-      'quality',
-      'placeholder',
-      'blurDataURL',
-      'loader ',
-      'layout',
-    ].includes(prop),
-});
+import Image, { ImageProps } from 'next/image';
 
 const components = {
-  h1: function h1(props: ChakraProps) {
-    return <Heading {...props} as="h1" size="4xl" />;
-  },
-  h2: function h2(props: ChakraProps) {
-    return <Heading {...props} as="h2" size="2xl" />;
-  },
-  h3: function h3(props: ChakraProps) {
-    return <Heading {...props} as="h3" size="xl" />;
-  },
-  h4: function h4(props: ChakraProps) {
-    return <Heading {...props} as="h4" size="lg" />;
-  },
-  h5: function h5(props: ChakraProps) {
-    return <Heading {...props} as="h5" size="md" />;
-  },
-  h6: function h6(props: ChakraProps) {
-    return <Heading {...props} as="h6" size="sm" />;
-  },
   img: function img(props: ImageProps) {
     return (
       <Image layout="responsive" loading="lazy" {...props} alt={props.alt} />
     );
-  },
-  ul: function ul(props: ChakraProps) {
-    return <UnorderedList {...props} />;
-  },
-  ol: function ol(props: ChakraProps) {
-    return <OrderedList {...props} />;
-  },
-  li: function li(props: ChakraProps) {
-    return <ListItem {...props} />;
   },
 };
 
