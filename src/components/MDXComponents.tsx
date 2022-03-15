d449400abf36bc708f9bb050ab9ffca275e2fa21
Alexandru Mircean <amircean@yahoo.com>
2022-03-15 17:55:28 +0200

    feat: content section update

--- src/components/MDXComponents.tsx
+++ src/components/MDXComponents.tsx
@@ -5,11 +5,39 @@
  */
 
 import Image, { ImageProps } from 'next/image';
+import Link from 'next/link';
+
+interface ElementProps {
+  id: string;
+  children: [JSX.Element];
+}
 
 const components = {
   img: function img(props: ImageProps) {
     return (
       <Image layout="responsive" loading="lazy" {...props} alt={props.alt} />
+    );
+  },
+  h2: function h2(props: ElementProps) {
+    return (
+      <>
+        <h2>{props.children}</h2>
+        <Link href={'/'} passHref>
+          <div className="divider-container">
+            <div className="divider"></div>
+            <div className="jump">Jump</div>
+            <div className="arrow-container">
+              <Image
+                src="/corner-right-up.svg"
+                className="arrow"
+                alt="arrow"
+                width={24}
+                height={24}
+              />
+            </div>
+          </div>
+        </Link>
+      </>
     );
   },
 };
