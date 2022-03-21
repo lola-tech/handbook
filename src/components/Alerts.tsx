fe12f561e391aeaf66d94257e1909105a7f695ab
Alexandru Mircean <amircean@yahoo.com>
2022-03-21 14:11:41 +0200

    fix: alerts

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,6 +1,9 @@
+import Image from 'next/image';
+
 const Alerts = ({ type, message }: { type: string; message: string }) => {
   return (
     <div className={`alert-container ${type}-container`}>
+      <Image src={`/icons/${type}.svg`} alt={type} width={40} height={40} />
       <div className={`alert-icon ${type}-alert-icon`}></div>
       <p>{message}</p>
     </div>
