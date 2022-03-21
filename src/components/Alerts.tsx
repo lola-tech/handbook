886b8709e7cbe82c3ecdeb370ddf8db9c698b622
Paul Ilies <pau.lilies@Paul-Ilies.local>
2022-03-21 14:36:10 +0200

    fix: alerts icon

--- src/components/Alerts.tsx
+++ src/components/Alerts.tsx
@@ -1,7 +1,22 @@
+import Image from 'next/image';
+import { useTheme } from '@/themeProvider/themeContext';
 const Alerts = ({ type, message }: { type: string; message: string }) => {
+  const { theme } = useTheme();
   return (
     <div className={`alert-container ${type}-container`}>
-      <div className={`alert-icon ${type}-alert-icon`}></div>
+      <div className={`alert-icon `}>
+        <Image
+          src={
+            theme === 'dark'
+              ? `/icons/${type}-dark.svg`
+              : `/icons/${type}-light.svg`
+          }
+          alt="icon"
+          width={40}
+          height={40}
+          className={`${type}-alert-icon`}
+        />
+      </div>
       <p>{message}</p>
     </div>
   );
